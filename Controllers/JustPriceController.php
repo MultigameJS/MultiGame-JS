0bace350cef4692527c2a2b4b2a6911161112b67
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-05 23:11:14 +0100

    créa du controller  du  jeu justPrice(quelques test effectués)

---
+++ Controllers/JustPriceController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Services\JustPriceService;
+
+class JustPriceController extends Controller
+{
+
+    public function Index()
+    {
+        $this->render("justPrice/index");
+    }
+
+    public function SaveScore() // verification du champ test1 et 2 (name dans mon input dans html)
+    {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") { // REQUEST_METHOD sert à = verifier si la requete de la methode est strictement egal a POST
+     // on place la super global post (la requete) dans ma variable $data
+            $justPriceService = new JustPriceService(); // permet d envoyer les donnees au service concerné (ou model si on utilise pas repo et service)
+            $justPriceService->SaveScore(); // on reprend $ data qui comporter tout les elements de mon POST ey le renvoyé au service
+            // utilisé la fonction dont on a besoin dans le service en question
+            // on renvoi la paire clé(status)/valeur(success ou error ou message) au fetch
+        } else { // RAPPEL pas de parametre () en else !
+            http_response_code(404);
+            echo json_encode(["status" => "error", "message" => "Ce n'est pas une requête POST"]); // si pas req POST messge error
+        }
+    
+    } 
+}
