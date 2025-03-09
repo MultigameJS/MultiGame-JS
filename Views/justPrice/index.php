8ccb7d11d0a290279e21de33d099571355691e92
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-09 23:09:02 +0100

    maj de l html du jeu

--- Views/justPrice/index.php
+++ Views/justPrice/index.php
@@ -1,3 +1,5 @@
+<body class="bodys">
+
 <h1>JUST PRICE</h1>
 
 <!-- BACKGROUND MAIN -->
@@ -7,26 +9,29 @@
 <!-- CARDS ITEMS JUST PRICE -->
 <div class="cards-container">
     <div class="card" id="card1">
-        <img src="/assets/images/justprice/iPhone.jpg" class="card-img-top" alt="iphone16">
-        <p class="heading">iPhone 16</p>
-        <input type="number" name="play1" id="play1" placeholder="Votre prix">
+        <img src="/assets/images/justprice/iPhone.jpg" class="card-img-top" alt="iphone">
+        <p class="heading">iPhone</p>
+        <input type="number" name="play1" id="play1" placeholder="Votre prix" class="d-none">
         <button id="button1" class="price-button">JOUER</button>
+        <div class="textPlay" id="textPlay"></div>
     </div>
     <div class="card" id="card2">
         <img src="/assets/images/justprice/tesla.jpg" class="card-img-top" alt="tesla">
         <p class="heading">Tesla Cybertruck</p>
-        <input type="number" name="play2" id="play2" placeholder="Votre prix">
+        <input type="number" name="play2" id="play2" placeholder="Votre prix" class="d-none">
         <button id="button2" class="price-button">JOUER</button> <!-- VOIR YO DATA-CARD POUR EVITER LES ID CE QU IL EN PENSE ?  -->
+        <div class="textPlay" id="textPlay"></div>
     </div>
     <div class="card" id="card3">
         <img src="/assets/images/justprice/Converse.jpg" class="card-img-top" alt="converse">
         <p class="heading">Converse Chuck 70</p>
-        <input type="number" name="play3" id="play3" placeholder="Votre prix">
+        <input type="number" name="play3" id="play3" placeholder="Votre prix" class="d-none">
         <button id="button3" class="price-button">JOUER</button>
+        <div class="textPlay" id="textPlay"></div>
     </div>
 </div>
 
-<script src="/assets/js/justprice/main.js"></script>
+</body>
 
 <?php
 $game = "justprice/main";
