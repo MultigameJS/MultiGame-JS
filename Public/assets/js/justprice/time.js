7c880cc3f6fe8f3184e779963197135369bcbacc
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-10 09:09:29 +0100

    separation des logiques pour plus de clareté et amélioré le jeu

---
+++ Public/assets/js/justprice/time.js
@@ -0,0 +1,1 @@
+// UTILISATION DE setTimeout()  ?? A VOIR 
--- Public/assets/js/justprice/verifJustPrice.js
+++ Public/assets/js/justprice/verifJustPrice.js
@@ -1,22 +1,23 @@
-// Fonction de vérification des entrées
+// VERIF DES ENTREES JOUEUR
 export function verifJustePrix(cards, valeur) {
-  if (valeur === "" || isNaN(valeur)) {
+    if (valeur === "" || isNaN(valeur)) {
     alert("Entrez un prix valide !");
     return;
-  }
+}
 
-  let card = justPriceGame[cards]; // JE RECUPERE MA CARTE DANS L OBJET DEFINI DE DEPART
-  let proposition = parseInt(valeur);
+let card = justPriceGame[cards]; // JE RECUPERE MA CARTE DANS L OBJET DEFINI DE DEPART
+let proposition = parseInt(valeur);
 
-  if (proposition === card.prix) {
-    let textPlayer = document.getElementById("textPlay"); // JE REMPLACE LES "ALERT"
+let textPlayer = document.getElementById("textPlay"); // JE REMPLACE LES "ALERT"
+let p = document.createElement("p");
 
-    let p = document.createElement("p");
-    textPlayer.appendChild(p);
+textPlayer.appendChild(p); // VOIR YO ALERT RETIRER TEXT MIS EN PLACE MAIS APPARAIT PAS...
+
+if (proposition === card.prix) {
     p.textContent = "Bravo, vous avez trouvé le juste prix !";
-  } else if (proposition < card.prix) {
+    } else if (proposition < card.prix) {
     p.textContent = "Trop bas, retentez votre chance";
-  } else {
-    p.textContent = "Trop haut, retentez votre chance";
-  }
+        } else {
+        p.textContent = "Trop haut, retentez votre chance";
+}
 }
