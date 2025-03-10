4a4bb7d209827cc0afb0e1907e4036a6415a241c
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-10 23:29:43 +0100

    ajout d'un timer pour le jeu

--- Public/assets/js/justprice/time.js
+++ Public/assets/js/justprice/time.js
@@ -1,1 +1,15 @@
-// UTILISATION DE setTimeout()  ?? A VOIR 
+// ON DEMARRE LE TIMER 
+export function startTimer(card) {
+    let textPlay = card.querySelector(".textPlay");
+    let seconds = 0;
+
+    card.timer = setInterval(function () {
+        seconds++;
+        textPlay.innerText = "Temps : " + seconds + " sec";
+    }, 1000);
+}
+
+// ON STOP LE TIMER SI RESPONSE OK CONSOLE
+export function stopTimer(card) {
+    clearInterval(card.timer);
+}
