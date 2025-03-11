4c476543b6ed50e0d196fe318794c09fc42b46e0
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-11 15:06:43 +0100

    MAJ du timer debug

--- Public/assets/js/justprice/time.js
+++ Public/assets/js/justprice/time.js
@@ -1,15 +1,23 @@
 // ON DEMARRE LE TIMER 
 export function startTimer(card) {
     let textPlay = card.querySelector(".textPlay");
-    let seconds = 0;
 
+    // Vérifier s'il existe déjà un élément pour le timer, sinon le créer
+    let timerDisplay = card.querySelector(".timer-display");
+    if (!timerDisplay) {
+        timerDisplay = document.createElement("p");
+        textPlay.prepend(timerDisplay); // Ajouter en haut de textPlay
+    }
+
+    let seconds = 0;
     card.timer = setInterval(function () {
         seconds++;
-        textPlay.innerText = "Temps : " + seconds + " sec";
+        timerDisplay.innerText = "Temps : " + seconds + " sec";
     }, 1000);
 }
 
-// ON STOP LE TIMER SI RESPONSE OK CONSOLE
+// ON STOP LE TIMER SI REPONSE OK
 export function stopTimer(card) {
     clearInterval(card.timer);
+    card.timer = null;
 }
