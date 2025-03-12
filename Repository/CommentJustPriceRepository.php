1bbd5c820e896bf081196d9edb271c85e5b798e5
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-12 19:03:14 +0100

    ajout de modif pour le NoSql

--- Repository/CommentJustPriceRepository.php
+++ Repository/CommentJustPriceRepository.php
@@ -5,29 +5,13 @@ namespace App\Repository;
 use MongoDB\BSON\ObjectId; // VOIR YO CAR FONCTIONNE PAS 
 use App\Repository\DbMongo;
 
-class CommentJustPriceRepository
+class CommentJustPriceRepository extends DbMongo
 {
-    private DbMongo $db;
-    private string $collection = "justpriceComments"; // NOM DE LA COLLECTION MONGODB
 
-    public function __construct()
+    public function table($data): string
     {
-        $this->db = new DbMongo(); // INSTANCIATION DE DBMONGO
+        return $this->create("justpriceComments",$data);
     }
 
-    public function save(array $data): string
-    {
-        return $this->db->create($this->collection, $data);
-    }
-
-    public function getAll(): array
-    {
-        return $this->db->findAll($this->collection);
-    }
-
-    public function deleteById(string $id): bool
-    {
-        return $this->db->delete($this->collection, ['_id' => $id]) > 0; 
-        // return $this->db->delete($this->collection, ['_id' => new ObjectId($id)]) > 0; // VOIR YO DEBUG CAR J AI VERIFIER PHP INI COMPOSER ET VERSION SUR MON PC
-    }
 }
+
