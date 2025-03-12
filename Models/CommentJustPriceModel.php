96f1af618723fa56c17cfadaf134b7c54e5f4862
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-12 19:00:50 +0100

    apport de modifications cote back du jeu pour le NoSql

--- Models/CommentJustPriceModel.php
+++ Models/CommentJustPriceModel.php
@@ -4,13 +4,56 @@ namespace App\Models;
 
 use App\Repository\DbMongo;
 
-class CommentJustPriceModel extends DbMongo
+class CommentJustPriceModel extends Model
 {
-    protected string $collection = "justpriceComments"; // COLLECTION MONGO DB
+    protected $id;
+    protected $pseudo;
+    protected $commentaire;
 
-    public function __construct()
-    {
-        parent::__construct(); // HERITAGE DU CONSTRUCTEUR DE DbMONGO
+
+    /**
+     * Get the value of id
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id): self {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Get the value of pseudo
+     */
+    public function getPseudo() {
+        return $this->pseudo;
+    }
+
+    /**
+     * Set the value of pseudo
+     */
+    public function setPseudo($pseudo): self {
+        $this->pseudo = $pseudo;
+        return $this;
+    }
+
+    /**
+     * Get the value of commentaire
+     */
+    public function getCommentaire() {
+        return $this->commentaire;
+    }
+
+    /**
+     * Set the value of commentaire
+     */
+    public function setCommentaire($commentaire): self {
+        $this->commentaire = $commentaire;
+        return $this;
     }
 }
 
