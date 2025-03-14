603196f522e9b85934b576a0f3c7e2533b703cdb
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-14 09:00:20 +0100

    maj data  pour debug envoi serveur

--- Services/JustPriceService.php
+++ Services/JustPriceService.php
@@ -13,13 +13,14 @@ class JustPriceService
         $justPriceModel = new JustPriceModel();
         $justPriceRepository = new JustPriceRepository();
 
-
-        if (empty($_POST["time"]) || empty($_POST["score"])) { // function isset = si variable existe et empty => si elle existe et si elle est vide
+// VOIR YOYO J AI ENLEVER TIME 
+        if (empty($_POST["score"])) { // function isset = si variable existe et empty => si elle existe et si elle est vide
             echo json_encode(["status" => "error", "message" => "veuillez compléter tous les champs"]); // status car statu actuel on verifie s'il est = à erreur. encode la reponse à afficher / "error" car si ne renvoi pas success 200 trame à respecter
+        // VOIR YO AJOUTER RETURN OU EXIST ?? SI UN CHAMP EST VIDE...
         }
 
         $score = floatval($_POST["score"]); // definie dans une variable pour indiquer que c est un float
-        $time = floatval($_POST["time"]);
+        // $time = floatval($_POST["time"]); VOIR YO SI OK POUR ENLEVER
         $userId = $_SESSION["id"];
 
         if ($score <= 0) {
@@ -28,12 +29,12 @@ class JustPriceService
             $data = [
                 "idUsers" => $userId,  
                 "score" => $score,
-                "time" => $time
+                "time" => $score // VOIR YO POUR LA MODIF DE TIME EN $SCORE CAR TIME = SCORE ? 
             ];
 
             $justPriceModel->hydrate($data); // car model qui hydrate
             $justPriceRepository->create($data); // car crud dans dbrepo
-            $justPriceRepository->update($userId, ["score" => $score, "time" => $time]);
+            $justPriceRepository->update($userId, ["score" => $score, "time" => $score]);
 
             echo json_encode(["status" => "success", "message" => "Votre Score est enregistré"]);
     }
