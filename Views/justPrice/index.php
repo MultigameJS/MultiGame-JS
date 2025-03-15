e181af567bc3849ed52d9a62e97ce25de1a11784
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-15 20:05:30 +0100

    derniere maj de la view pour le front

--- Views/justPrice/index.php
+++ Views/justPrice/index.php
@@ -2,38 +2,32 @@
 
     <h1>JUST PRICE</h1>
 
-    <!-- BACKGROUND MAIN -->
-    <div class="container justp">
-
         <!-- CARDS ITEMS JUST PRICE -->
-    <div class="container justp">
+        <div class="container justp">
         <div class="cards-container">
-        <div class="card" id="card3" data-price="">
-    <p class="click-text">-> CLIC <-</p>
-    <img src="/assets/images/justprice/Converse.jpg" class="card-img-top" alt="converse">
-    <p class="heading">Converse Chuck 70 <br> Niveau Facile </p>
-    <input type="number" id="play3" placeholder="Votre prix" class="hidden">
-    <button id="button3" class="price-button hidden">Envoyer</button>
-    <div class="textPlay"></div>
-</div>
+            <div class="card" id="card3">
+                <p class="click-text">-> CLIC <-</p>
+                <img src="/assets/images/justprice/Converse.jpg" alt="converse">
+                <p class="heading">Converse Chuck 70 <br> Niveau Facile</p>
+                <input type="number" id="play3" placeholder="Votre prix" class="hidden">
+                <button id="button3" class="price-button hidden">Envoyer</button>
+            </div>
 
-<div class="card" id="card1" data-price="">
-    <p class="click-text">-> CLIC <-</p>
-    <img src="/assets/images/justprice/iphoneia.jpg" class="card-img-top" alt="iphone">
-    <p class="heading">iPhone <br> Niveau Moyen </p>
-    <input type="number" id="play1" placeholder="Votre prix" class="hidden">
-    <button id="button1" class="price-button hidden">Envoyer</button>
-    <div class="textPlay"></div>
-</div>
+            <div class="card" id="card1">
+                <p class="click-text">-> CLIC <-</p>
+                <img src="/assets/images/justprice/iphoneia.jpg" alt="iphone">
+                <p class="heading">iPhone <br> Niveau Moyen</p>
+                <input type="number" id="play1" placeholder="Votre prix" class="hidden">
+                <button id="button1" class="price-button hidden">Envoyer</button>
+            </div>
 
-<div class="card" id="card2" data-price="">
-    <p class="click-text">-> CLIC <-</p>
-    <img src="/assets/images/justprice/tesla.jpg" class="card-img-top" alt="tesla">
-    <p class="heading">Tesla Cybertruck <br> Niveau Difficile </p>
-    <input type="number" id="play2" placeholder="Votre prix" class="hidden">
-    <button id="button2" class="price-button hidden">Envoyer</button>
-    <div class="textPlay"></div>
-</div>
+            <div class="card" id="card2">
+                <p class="click-text">-> CLIC <-</p>
+                <img src="/assets/images/justprice/tesla.jpg" alt="tesla">
+                <p class="heading">Tesla Cybertruck <br> Niveau Difficile</p>
+                <input type="number" id="play2" placeholder="Votre prix" class="hidden">
+                <button id="button2" class="price-button hidden">Envoyer</button>
+            </div>
         </div>
     </div>
 
